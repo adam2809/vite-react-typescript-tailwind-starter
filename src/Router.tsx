e98ba3378fb60c6cfc50d789d1f80113b1782927
Adam Kulesza <adam.kule@gmail.com>
2025-04-22 02:18:11 +0200

    feat: Wraps router inside of a card

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,39 +1,39 @@
 import React from 'react'
-import {
-  Link,
-  Route,
-  Routes,
-  useLocation
-} from 'react-router-dom'
+import { Link, Route, Routes, useLocation } from 'react-router-dom'
 
 import { Button } from './components/ui/button'
 
 import FadeAnimation from './components/fade-animation'
 import { CarouselPage } from './pages/carousel-page'
+import { Card } from './components/ui/card'
 
 const Router: React.FC = () => {
   const location = useLocation()
   return (
-    <Routes location={location} key={location.pathname}>
-      <Route
-        path="/"
-        element={
-          <FadeAnimation>
-            <CarouselPage />
-          </FadeAnimation>
-        }
-      />
-      <Route
-        path="/categories"
-        element={
-          <FadeAnimation>
-            <Link to="/">
-              <Button className='text-2xl py-8 px-6 uppercase font-serif tracking-widest'>Home</Button>
-            </Link>
-          </FadeAnimation>
-        }
-      />
-    </Routes>
+    <Card className="p-3 h-9/10">
+      <Routes location={location} key={location.pathname}>
+        <Route
+          path="/"
+          element={
+            <FadeAnimation>
+              <CarouselPage />
+            </FadeAnimation>
+          }
+        />
+        <Route
+          path="/categories"
+          element={
+            <FadeAnimation>
+              <Link to="/">
+                <Button className="text-2xl py-8 px-6 uppercase font-serif tracking-widest">
+                  Home
+                </Button>
+              </Link>
+            </FadeAnimation>
+          }
+        />
+      </Routes>
+    </Card>
   )
 }
 
